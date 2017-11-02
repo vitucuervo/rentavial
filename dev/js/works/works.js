e8d4042fbf4f1bb52def25d4e6c77455423df99a
Silvio Vitullo <silviovitullo@gmail.com>
2017-11-02 02:15:33 +0100

    Final images and content

--- dev/js/works/works.js
+++ dev/js/works/works.js
@@ -1,8 +1,11 @@
 $(document).on('ready', function(){
 
+	// Deshabilitado hasta tener el contenido
+    /*
     $('.work-item').click(function(){
         $(this).toggleClass('is-expanded')
-    });    
+    });
+    */    
     
 });
 
